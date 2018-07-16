02461424bab1b48a005a79966f5a3e7680fce2cb
Benjamin <benjaminsalasek@Athena.local>
2018-07-16 15:32:47 -0500

    navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GemComponent } from './gem/gem.component';
 import { WavePipe } from './wave.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GemComponent,
-    WavePipe
+    WavePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
